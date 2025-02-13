ae92b516555019de0fd64348d016e775e24f4832
Christopher <gomeschristopher@hotmail.com>
2025-02-13 08:29:53 -0300

    list user

--- src/main/java/com/gomeschristopher/order/OrderApplication.java
+++ src/main/java/com/gomeschristopher/order/OrderApplication.java
@@ -3,6 +3,8 @@ package com.gomeschristopher.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.persistence.Table;
+
 @SpringBootApplication
 public class OrderApplication {
 
---
+++ src/main/java/com/gomeschristopher/order/services/UserService.java
@@ -0,0 +1,27 @@
+package com.gomeschristopher.order.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gomeschristopher.order.entities.User;
+import com.gomeschristopher.order.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		
+		return user.get();
+	}
+}
